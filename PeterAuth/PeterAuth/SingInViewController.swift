26f27675502022a1faa68935605b08dfb4cd46f2
PeterKlim <72018742+PeterKlim@users.noreply.github.com>
2021-03-23 18:18:16 +0300

    Add files via upload

---
+++ PeterAuth/PeterAuth/SingInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SingInViewController.swift
+//  PeterAuth
+//
+//  Created by user on 23.03.2021.
+//
+
+import UIKit
+
+class SingInViewController: UIViewController {
+
+    @IBOutlet weak var SingButton: UIButton!
+    
+    @IBOutlet weak var Text: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
